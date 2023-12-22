89efa8d95016921fa07d1149ba2d660e010dffba
tonikelope <tonikelope@gmail.com>
2023-12-22 15:50:56 +0100

    4.96

--- plugin.video.alfa/channels/omega.py
+++ plugin.video.alfa/channels/omega.py
@@ -37,7 +37,7 @@ from datetime import datetime
 
 CHECK_STUFF_INTEGRITY = True
 
-OMEGA_VERSION = "4.95"
+OMEGA_VERSION = "4.96"
 
 config.set_setting("unify", "false")
 
@@ -659,7 +659,7 @@ def update_watchdog_episodes(item_url, new_count):
     EPISODE_WATCHDOG[item_url]=new_count
 
     if int(episodios)<int(new_count):
-        xbmcgui.Dialog().notification('OMEGA ' + OMEGA_VERSION, "["+str(new_count-episodios)+"] EPISODIOS NUEVOS", os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'media', 'channels', 'thumb', 'omega.gif'), 5000)
+        xbmcgui.Dialog().notification('OMEGA ' + OMEGA_VERSION, "["+str(int(new_count)-int(episodios))+"] EPISODIOS NUEVOS", os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'media', 'channels', 'thumb', 'omega.gif'), 5000)
 
     with open(KODI_NEI_EPISODE_WATCHDOG_PATH, "w+") as file:
         for k in EPISODE_WATCHDOG.keys():
