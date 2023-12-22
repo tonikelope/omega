e8873c244aed0440fc3669fbaf88c6b9db465700
tonikelope <tonikelope@gmail.com>
2023-12-22 17:20:04 +0100

    5.1

--- plugin.video.alfa/channels/omega.py
+++ plugin.video.alfa/channels/omega.py
@@ -37,7 +37,7 @@ from datetime import datetime
 
 CHECK_STUFF_INTEGRITY = True
 
-OMEGA_VERSION = "5.0"
+OMEGA_VERSION = "5.1"
 
 config.set_setting("unify", "false")
 
@@ -661,7 +661,7 @@ def update_watchdog_episodes(item_url, new_count, serie_name):
     nuevos = int(new_count)-int(episodios)
 
     if int(episodios)<int(new_count):
-        xbmcgui.Dialog().ok('OMEGA ' + OMEGA_VERSION + ' (by tonikelope)', ("HAY ("str(nuevos)+ ") EPISODIOS NUEVOS de\n\n" if nuevos > 1 else "HAY NUEVO EPISODIO de\n\n")+serie_name)
+        xbmcgui.Dialog().ok('OMEGA ' + OMEGA_VERSION + ' (by tonikelope)', ("HAY ("+str(nuevos)+") EPISODIOS NUEVOS de\n\n" if nuevos > 1 else "HAY NUEVO EPISODIO de\n\n")+serie_name)
         
     with open(KODI_NEI_EPISODE_WATCHDOG_PATH, "w+") as file:
         for k in EPISODE_WATCHDOG.keys():
