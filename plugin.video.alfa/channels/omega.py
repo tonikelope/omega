87364a1f16754a3bd3889c7029803243687b2554
tonikelope <tonikelope@gmail.com>
2025-01-15 19:48:26 +0100

    ...

--- plugin.video.alfa/channels/omega.py
+++ plugin.video.alfa/channels/omega.py
@@ -154,7 +154,7 @@ FORO_ITEMS_RETRY = 3
 MAX_URL_RETRIEVE_ERROR = 2
 
 DEFAULT_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
 
 FOROS_FINALES_NEI = [
